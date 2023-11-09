91c448f7035d229f3dfb69bf1fd94b9f619ff98b
Luke Gorrie <luke@nuddy.net>
2023-11-09 11:45:07 +0100

    runtests.jl: Add example from thesis
    Run the example from page 67 of the BART.jl thesis
    https://iro.uiowa.edu/esploro/outputs/doctoral/MCMC-diagnostics-for-Bayesian-additive-regression/9983988297702771

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,19 @@
 using BART
+using Distributions
+using MCMCChains
 using Test
 
 @testset "BART.jl" begin
-    # Write your own tests here.
+    # Example from the thesis.
+    #using StatsPlots
+    g(x) = 10sin.(pi * x[1] .* x[2]) + 20(x[3] .- 0.5).^2 + 10x[4] + 5x[5]
+    n = 250
+    p = 10
+    σ = sqrt(1)
+    X = rand(n, p)
+    y = vec(mapslices(g, X, dims = 2)) + rand(Normal(0, σ), n)
+    post = fit(BartModel, X, y)
+    monitor = Chains(post)
+    gelmandiag(monitor)
+    #plot(monitor)
 end
